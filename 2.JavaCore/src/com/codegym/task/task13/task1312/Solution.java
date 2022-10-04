51e85fea5e07ea14c41e3b9ad1e4ac01c6b2ae76
Michal <sasxcxc@icloud.com>
2022-10-04 20:06:41 +0200

    Exercise done by me. Michal Francuz , francuz.michal@icloud.com

---
+++ 2.JavaCore/src/com/codegym/task/task13/task1312/Solution.java
@@ -0,0 +1,14 @@
+package com.codegym.task.task13.task1312;
+
+public class Solution {
+    public static void main(String[] args) {
+        Translator translator = new Translator();
+        System.out.println(translator.translate());
+    }
+
+    public static class Translator {
+        public String translate() {
+            return "I translate from Russian";
+        }
+    }
+}
