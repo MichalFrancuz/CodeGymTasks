7ed7ce67e218ba9422fea2e634bb632114c2ddc3
Michal <sasxcxc@icloud.com>
2022-10-06 17:08:45 +0200

    Exercise done by me. Michal Francuz , francuz.michal@icloud.com

--- 8.Games/src/com/codegym/games/minesweeper/GameObject.java
+++ 8.Games/src/com/codegym/games/minesweeper/GameObject.java
@@ -2,9 +2,10 @@ package com.codegym.games.minesweeper;
 
 public class GameObject {
     public int x, y;
-
-    public GameObject(int i, int j) {
+    public boolean isMine;
+    public GameObject(int i, int j, boolean k) {
         this.x = i;
         this.y = j;
+        this.isMine = k;
     }
 }
--- 8.Games/src/com/codegym/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/codegym/games/minesweeper/MinesweeperGame.java
@@ -4,6 +4,7 @@ import com.codegym.engine.cell.*;
 
 
 public class MinesweeperGame extends Game {
+    private int countMinesOnField = 0;
     private static final int SIDE = 9;
     private final GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
@@ -13,11 +14,19 @@ public class MinesweeperGame extends Game {
     }
 
     private void createGame() {
+        int countMine = 0;
         for (int i = 0; i < SIDE; i++) {
             for (int j = 0; j < SIDE; j++) {
-                gameField[i][j] = new GameObject(j, i);
+                int n = getRandomNumber(10);
+                boolean isMineMG = false;
+                if (n == 9) {
+                    isMineMG = true;
+                    countMine++;
+                }
+                gameField[i][j] = new GameObject(j, i, isMineMG);
                 setCellColor(j, i, Color.ORANGE);
             }
         }
+        countMinesOnField = countMine;
     }
 }
